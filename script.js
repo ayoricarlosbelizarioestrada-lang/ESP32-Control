47df06f306ad63b2fd41e5ae8197b58218f70ca8
ayoricarlosbelizarioestrada-lang <ayoricarlosbelizarioestrada@gmail.com>
2026-08-06 21:09:37 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+document.getElementById("btnOn").onclick=function(){
 
+    document.querySelector("#estado span").innerHTML="🟢 Encendido";
+
+}
+
+document.getElementById("btnOff").onclick=function(){
+
+    document.querySelector("#estado span").innerHTML="🔴 Apagado";
+
+}
